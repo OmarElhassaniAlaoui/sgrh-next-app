2d690e30740fab6bf54bf6e1ba1e86a4e511fd0e
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2025-04-16 20:34:05 +0100

    migrat from sqlite to postgres

--- src/app/api/employees/route.ts
+++ src/app/api/employees/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma";
 
 const prisma = new PrismaClient();
 
